989e5ee65611bcc1dfcfb1a9775b8515eae69907
Francisco De La Cruz <helloquico@gmail.com>
2015-09-17 14:10:59 -0400

    Add gulp-connect and gulp-open

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,27 @@
 var gulp = require('gulp');
 var browserify = require('gulp-browserify');
 var concat = require('gulp-concat');
+var connect = require('gulp-connect');
+var open = require('gulp-open');
+
+var config = {
+	port: 4000,
+	devBaseUrl: 'http://localhost'
+};
+
+gulp.task('connect', function () {
+	connect.server({
+		root: './dist',
+		port: config.port,
+		base: config.devBaseUrl,
+		livereload: true
+	});
+});
+
+gulp.task('open', ['connect'], function () {
+	gulp.src('dist/index.html')
+			.pipe(open({ uri: config.devBaseUrl + ':' + config.port + '/' }));
+});
 
 gulp.task('browserify', function () {
 	gulp.src('src/js/main.js')
@@ -14,7 +35,7 @@ gulp.task('copy', function () {
 			.pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['browserify', 'copy']);
+gulp.task('default', ['browserify', 'copy', 'open']);
 
 gulp.task('watch', function () {
 	gulp.watch('src/**/*.*', ['default']);
